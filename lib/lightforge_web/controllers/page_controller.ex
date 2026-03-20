190814773f3d592b7038ab6aa60950c33bb658dd
Xam <sting0l0@protonmail.com>
2026-03-20 01:07:17 -0400

    Chore: rewrite homepage for Lightforge

--- lib/lightforge_web/controllers/page_controller.ex
+++ lib/lightforge_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule LightforgeWeb.PageController do
   use LightforgeWeb, :controller
 
+  alias Lightforge.BattleNet
+  alias LightforgeWeb.BattleNetSession
+
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home,
+      battle_net_connected: BattleNetSession.connected?(conn),
+      battle_net_ready: BattleNet.ready?()
+    )
   end
 end
--- test/lightforge_web/controllers/page_controller_test.exs
+++ test/lightforge_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LightforgeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "A cleaner way to inspect your retail character"
   end
 end
